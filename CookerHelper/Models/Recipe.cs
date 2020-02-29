65d20cb5d568ba4ffe1d3173a1ba3aa732cb9204
Stepan <stepansmetanskyy@gmail.com>
2020-02-29 23:33:29 +0200

    Add Recipe class

---
+++ CookerHelper/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookerHelper.Models
+{
+    [Table("tblRecipes")]
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(200)]
+        public string Name { get; set; }
+        [Required]
+        public string Directions { get; set; }
+        public int PrepareTime { get; set; }
+        public int CookTime { get; set; }
+        public float Rate { get; set; }
+        public int Vote { get; set; }
+    }
+}
